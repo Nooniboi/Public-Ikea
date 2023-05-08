865021f985aa6eff3f91e46b30ee3084ac86db52
vined <115558899+vined-underscore@users.noreply.github.com>
2023-05-08 18:30:08 +0200

    0.2.0

--- src/main/java/org/vined/ikea/IKEA.java
+++ src/main/java/org/vined/ikea/IKEA.java
@@ -2,17 +2,19 @@ package org.vined.ikea;
 
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.systems.modules.Category;
+import meteordevelopment.meteorclient.systems.commands.Commands;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 
 import com.mojang.logging.LogUtils;
 
 import org.slf4j.Logger;
 
-import org.vined.ikea.modules.misc.IKEAV3;
+import org.vined.ikea.modules.misc.IKEADupe;
 import org.vined.ikea.modules.misc.PacketLogger;
 import org.vined.ikea.modules.utility.AutoItemMove;
 import org.vined.ikea.modules.utility.DubCounter;
 import org.vined.ikea.modules.utility.NoChestRender;
+import org.vined.ikea.commands.ShulkerCounterCommand;
 
 
 public class IKEA extends MeteorAddon {
@@ -21,18 +23,19 @@ public class IKEA extends MeteorAddon {
     public static final Category MISC = new Category("IMisc");
 
 
-
-
     @Override
     public void onInitialize() {
         LOG.info("Initializing IKEA Addon");
 
         // Modules
-        Modules.get().add(new IKEAV3());
+        Modules.get().add(new IKEADupe());
         Modules.get().add(new NoChestRender());
         Modules.get().add(new DubCounter());
         Modules.get().add(new AutoItemMove());
         Modules.get().add(new PacketLogger());
+
+	// Commands
+	Commands.get().add(new ShulkerCounterCommand());
     }
 
     @Override
