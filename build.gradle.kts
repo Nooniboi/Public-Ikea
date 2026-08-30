ebcb0c7e83f0dbafeff2744f7f3873ba02e37429
Nooniboi <65405548+Nooniboi@users.noreply.github.com>
2026-08-30 19:58:06 +0200

    26.2 build changes Not a required change, but I prefer to follow the meteor-template.

---
+++ build.gradle.kts
@@ -0,0 +1,91 @@
+plugins {
+    alias(libs.plugins.fabric.loom)
+}
+
+val archivesBaseName = providers.gradleProperty("archives_base_name").get()
+val mavenGroup = providers.gradleProperty("maven_group").get()
+
+base {
+    archivesName = archivesBaseName
+    version = libs.versions.mod.version.get()
+    group = mavenGroup
+}
+
+repositories {
+    maven {
+        name = "meteor-maven"
+        url = uri("https://maven.meteordev.org/releases")
+    }
+    maven {
+        name = "meteor-maven-snapshots"
+        url = uri("https://maven.meteordev.org/snapshots")
+    }
+}
+
+dependencies {
+    // Fabric
+    minecraft(libs.minecraft)
+    implementation(libs.fabric.loader)
+
+    // Meteor
+    implementation(libs.meteor.client)
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(libs.versions.jdk.get().toInt()))
+    }
+}
+
+fun toMinecraftCompat(version: String): String {
+    val stable = Regex("""^(\d{2})\.([1-9]\d*)(?:\.(\d+))?$""")
+
+    stable.matchEntire(version)?.let {
+        val (year, drop, _) = it.destructured
+        return "~$year.$drop"
+    }
+
+    val pre = Regex("""^(\d{2})\.([1-9]\d*)-pre[-.](\d+)$""")
+    pre.matchEntire(version)?.let {
+        return version.replace("-pre-", "-pre.")
+    }
+
+    val rc = Regex("""^(\d{2})\.([1-9]\d*)-rc[-.](\d+)$""")
+    rc.matchEntire(version)?.let {
+        return version.replace("-rc-", "-rc.")
+    }
+
+    return version
+}
+
+tasks {
+    processResources {
+        val propertyMap = mapOf(
+            "version" to project.version,
+            "minecraft_version" to toMinecraftCompat(libs.versions.minecraft.get()),
+            "jdk_version" to libs.versions.jdk.get(),
+        )
+
+        inputs.properties(propertyMap)
+        filesMatching("fabric.mod.json") {
+            expand(propertyMap)
+        }
+    }
+
+    jar {
+        inputs.property("archivesName", archivesBaseName)
+
+        from("LICENSE") {
+            rename { "${it}_$archivesBaseName" }
+        }
+    }
+
+    withType<JavaCompile>().configureEach {
+        options.compilerArgs.addAll(
+            listOf(
+                "-Xlint:deprecation",
+                "-Xlint:unchecked"
+            )
+        )
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        maven {
+            name = "Fabric"
+            url = uri("https://maven.fabricmc.net/")
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+rootProject.name = "ikea-addon"
